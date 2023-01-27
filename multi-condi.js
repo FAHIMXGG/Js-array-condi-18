78cfc4b50737a4a8771f531852b3a3caf7aeaeb6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-27 17:44:49 +0600

    data

---
+++ multi-condi.js
@@ -0,0 +1,32 @@
+var isGraduated = false;
+var salary = 53000;
+var car = 1;
+if(isGraduated){
+    console.log("biya done");
+}
+// if (isGraduated == true){
+//     console.log("bibaho");
+// }
+// else{
+//     console.log("biya koira lav nai");
+// }
+
+// if (isGraduated == true && salary > 50000){
+//     console.log("bibaho");
+// }
+// else{
+//     console.log("biya koira lav nai");
+// }
+if (isGraduated == true && salary > 50000 && car >=1){
+    console.log("bibaho");
+}
+else{
+    console.log("biya koira lav nai");
+}
+
+if(isGraduated === true || salary > 150000){
+    console.log("lets buy a car");
+}
+else{
+    console.log("hoga mara khau");
+}
