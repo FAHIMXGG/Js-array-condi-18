114700b175168a16b711345dddfe5be85ed3decb
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-27 19:20:41 +0600

    d

---
+++ multi-stage.js
@@ -0,0 +1,14 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toast = 20;
+
+if (danishPrice < money){
+    console.log("danish");
+}
+else if(butterBread < money){
+    console.log(butterBread);
+}
+else {
+    console.log('none');
+}
