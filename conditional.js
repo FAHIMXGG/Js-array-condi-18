e33d70956982e0fcf5d9c3d36b0112ad091a9a14
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-27 16:14:31 +0600

    3rd

---
+++ conditional.js
@@ -0,0 +1,28 @@
+var phonePrice = 103000;
+var myBudget = 110000;
+if (phonePrice < myBudget){
+    console.log('I just buy');
+}
+// else (phonePrice > myBudget){
+//     console.log("I cant buy");
+// }
+
+var price = 500;
+var myMoney = 556;
+var myMoney2 = 50;
+if (price <= myMoney){
+    console.log("I just buy chicken");
+}
+// if (price > myMoney){
+//     console.log("i need money");
+// }
+else{
+    console("i need money")
+}
+
+if (price < myMoney2){
+    console.log("i will eat chicken");
+}
+else {
+    console.log("I will eat potato");
+}
